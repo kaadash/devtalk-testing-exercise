edc2aa70b6076080a3eb1cedc66399568bd1ce57
Artur Kudel <arturkudel@gmail.com>
2017-04-19 11:18:09 +0200

    Add averageValue component tests

---
+++ app/routes/home/averageValue/averageValue.component.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { FormattedMessage } from 'react-intl';
+import AverageValue from './averageValue.component';
+
+describe('Simple: Component', () => {
+  const defaultProps = {
+    arithmeticAverage: 22,
+  };
+
+  const component = (props = {}) => (
+    <AverageValue {...defaultProps} {...props} />
+  );
+
+  it('should render with average-value class', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.average-value'))
+      .to.have.length(1);
+  });
+
+  describe('Message Rendering: ', () => {
+    it('should render message', () => {
+      const wrapper = shallow(component());
+      expect(wrapper.find(FormattedMessage))
+        .to.have.length(1);
+    });
+  });
+});
